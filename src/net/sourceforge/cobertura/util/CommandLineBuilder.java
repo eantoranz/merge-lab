9fec1b0d1ae4cc237dbecc89d1629d262f9f8758
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2016-10-29 11:09:36 -0600

    Added a small coding detail and reformatted the file

--- src/net/sourceforge/cobertura/util/CommandLineBuilder.java
+++ src/net/sourceforge/cobertura/util/CommandLineBuilder.java
@@ -71,87 +71,88 @@ import java.util.List;
  * @author Grzegorz Lukasik
  */
 public class CommandLineBuilder {
-private static final String LINESEP = System.getProperty("line.separator");
+	private static final String LINESEP = System.getProperty("line.separator");
 
-	 // File that will be used to store arguments
-	  private File commandLineFile = null;
+	// File that will be used to store arguments
+	private File commandLineFile = null;
 
-	  // Writer that will be used to write arguments to the file
-	   private FileWriter commandLineWriter = null;
-
-			/**
-			 * Creates a new instance of the builder. Instances of this class should not
-			 * be reused to create many command lines.
-			 * 
-			 * @throws IOException
-			 *             if problems with creating temporary file for storing command
-			 *             line occur
-			 */
-			public CommandLineBuilder() throws IOException {
-				  commandLineFile = File.createTempFile("cobertura.", ".cmdline");
-			commandLineFile.deleteOnExit();
-			commandLineWriter = new FileWriter(commandLineFile);
-				}
+	// Writer that will be used to write arguments to the file
+	private FileWriter commandLineWriter = null;
 
 	/**
-	 		* Adds command line argument. Each argument can be thought as a single cell
-	 * in array passed to main method. This method should not be used after
-	 			* arguments were saved.
+	 * Creates a new instance of the builder. Instances of this class should not
+	 * be reused to create many command lines.
 	 * 
-	 * @param arg command line argument to save
-	 			* @throws IOException
-	 *          					   if problems with temporary file occur
-	 * 			@throws NullPointerException 
-	 *             							if <code>arg is null            
+	 * @throws IOException
+	 *             if problems with creating temporary file for storing command
+	 *             line occur
 	 */
-		public void addArg(String arg) throws IOException {
-	if( arg==null)
-		throw new NullPointerException();
-			commandLineWriter.write(arg + LINESEP);}
+	public CommandLineBuilder() throws IOException {
+		commandLineFile = File.createTempFile("cobertura.", ".cmdline");
+		commandLineFile.deleteOnExit();
+		commandLineWriter = new FileWriter(commandLineFile);
+	}
 
-	
-	     /**
-	 * Adds two command line arguments. Convienience function, calls
-	 *    {@link #addArg(String)} two times.   
+	/**
+	 * Adds command line argument. Each argument can be thought as a single cell
+	 * in array passed to main method. This method should not be used after
+	 * arguments were saved.
 	 * 
-	 * @param  arg1     first command     line argument to save
-	 * @param arg2 second command line argument to save
+	 * @param arg
+	 *            command line argument to save
 	 * @throws IOException
 	 *             if problems with temporary file occur
-	 * @throws NullPointerException 
-	 *             if any <code>arg is null            
+	 * @throws NullPointerException
+	 *             if <code>arg is null
 	 */
-    public void addArg(String arg1, String arg2) throws IOException {
-    	
-    	
-    	
-        addArg(arg1);
-        addArg(arg2);}
+	public void addArg(String arg) throws IOException {
+		if (arg == null)
+			throw new NullPointerException();
+		commandLineWriter.write(arg + LINESEP);
+	}
 
-    
-		/**
-		 * Saves options and made file available to use. Use method
-		 * {@link #getCommandLineFile} to get the file the arguments are saved in.
-		 * 
-		 * @throws IOException
-		 *             if problems with temporary file occur
-		 */
-		public void saveArgs() throws IOException {
+	/**
+	 * Adds two command line arguments. Convienience function, calls
+	 * {@link #addArg(String)} two times.
+	 * 
+	 * @param arg1
+	 *            first command line argument to save
+	 * @param arg2
+	 *            second command line argument to save
+	 * @throws IOException
+	 *             if problems with temporary file occur
+	 * @throws NullPointerException
+	 *             if any <code>arg is null
+	 */
+	public void addArg(String arg1, String arg2) throws IOException {
+
+		addArg(arg1);
+		addArg(arg2);
+	}
+
+	/**
+	 * Saves options and made file available to use. Use method
+	 * {@link #getCommandLineFile} to get the file the arguments are saved in.
+	 * 
+	 * @throws IOException
+	 *             if problems with temporary file occur
+	 */
+	public void saveArgs() throws IOException {
 		commandLineWriter.flush();
-			  commandLineWriter.close();
-		  }
+		commandLineWriter.close();
+	}
 
 	/**
 	 * Gets absolute path to the file with saved arguments. Notice, that however
 	 * this method can be used as soon as an instance of this class is created,
-	 * arguments should be read from the file after a call to
-	 * {@link #saveArgs} method.
+	 * arguments should be read from the file after a call to {@link #saveArgs}
+	 * method.
 	 * 
 	 * @return absolute path to the file with arguments
 	 */
-	  public String getCommandLineFile() {
+	public String getCommandLineFile() {
 		return commandLineFile.getAbsolutePath();
-	  }
+	}
 
 	/**
 	 * Explicity frees all resources associated with this instance. Result of
@@ -159,67 +160,65 @@ private static final String LINESEP = System.getProperty("line.separator");
 	 * unspecified.
 	 */
 	public void dispose() {
-	    commandLineFile.delete();
+		commandLineFile.delete();
 	}
 
-		/**
-		 * Loads arguments from file if <code>--commandsfile option is used. Checks
-		 * if passed array contains <code>--commandsfile String, and if
-		 * so arguments from file specified in the very next array cell are read. If
-		 * there are more then one <code>--commandsfile the result is unspecified. 
-		 *
-		 * @return The list of arguments read from commandsfile, or
-		 *         <code>args if commandsfile option was not specified
-		 *         or the file cannot be read.
-		 * @throws NullPointerException if args is null, or any argument is null
-		 * @throws IllegalArgumentException if --commandsfile is specified as last option
-		 * @throws IOException if I/O related error with temporary command line file occur
-		 */
-		public static String[] preprocessCommandLineArguments(String[] args)
-				throws IOException {
+	/**
+	 * Loads arguments from file if <code>--commandsfile option is used. Checks
+	 * if passed array contains <code>--commandsfile String, and if
+	 * so arguments from file specified in the very next array cell are read. If
+	 * there are more then one <code>--commandsfile the result is unspecified.
+	 *
+	 * @return The list of arguments read from commandsfile, or <code>args if
+	 *         commandsfile option was not specified or the file cannot be read.
+	 * @throws NullPointerException
+	 *             if args is null, or any argument is null
+	 * @throws IllegalArgumentException
+	 *             if --commandsfile is specified as last option
+	 * @throws IOException
+	 *             if I/O related error with temporary command line file occur
+	 */
+	public static String[] preprocessCommandLineArguments(String[] args)
+			throws IOException {
 		boolean hasCommandsFile = false;
 		String commandsFileName = null;
-		for (int i=0;i<args.length;i++) {
+		for (int i = 0; i < args.length; i++) {
 			if (args[i].equals("--commandsfile")) {
-				if(i==args.length-1) {
-					throw new IllegalArgumentException("'--commandsfile' specified as last option.");
+				if (i == args.length - 1) {
+					throw new IllegalArgumentException(
+							"'--commandsfile' specified as last option.");
 				}
-					hasCommandsFile=true;
-				commandsFileName=args[++i];
-				}
-				}
+				hasCommandsFile = true;
+				commandsFileName = args[++i];
+			}
+		}
 
-	    if (hasCommandsFile)
-		{
-			List arglist=new ArrayList();
-			BufferedReader bufferedReader=null;
+		if (hasCommandsFile) {
+			List arglist = new ArrayList();
+			BufferedReader bufferedReader = null;
 
-			try
-			{
-				bufferedReader=new BufferedReader(new FileReader(commandsFileName));
+			try {
+				bufferedReader = new BufferedReader(new FileReader(
+						commandsFileName));
 				String line;
 
-				while ((line = bufferedReader.readLine())!=null)
+				while ((line = bufferedReader.readLine()) != null)
 					arglist.add(line);
 
-			}
-			catch (IOException e)
-			{
-				throw new IOException( "Unable to read temporary commands file " + commandsFileName + ".");
-			}
-			finally
-			{
-				if(bufferedReader!=null) {
+			} catch (IOException e) {
+				throw new IOException("Unable to read temp commands file "
+						+ commandsFileName + ".");
+			} finally {
+				if (bufferedReader != null) {
 					try {
 						bufferedReader.close();
+					} catch (IOException e) {
 					}
-					catch (IOException e)
-					{}
 				}
 			}
 
-		args = (String[]) arglist.toArray(new String[arglist.size()]);
-			}
+			args = (String[]) arglist.toArray(new String[arglist.size()]);
+		}
 		return args;
 	}
 }
