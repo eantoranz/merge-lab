87361bb8b8cb54983c3c5a002ac7659ab45808b9
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2016-11-01 15:50:21 -0600

    A small change

--- src/net/sourceforge/cobertura/util/CommandLineBuilder.java
+++ src/net/sourceforge/cobertura/util/CommandLineBuilder.java
@@ -125,6 +125,7 @@ public class CommandLineBuilder {
     public void addArg(String arg1, String arg2) throws IOException {
         addArg(arg1);
         addArg(arg2);
+        saveArgs();
     }
 
     
